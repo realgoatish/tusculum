d5a196abfe25ec6ca76e62f44c8672c35fb85c92
Michael Lamb <mflambjr@gmail.com>
2022-07-26 01:42:04 -0400

    early tests

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,2 +1,57 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang='ts'>
+
+  import Section from '$prod/components/Section.svelte'
+  import Article from '$prod/components/Article.svelte'
+  import Heading from '$prod/components/Heading.svelte'
+
+</script>
+
+<main>
+  <Heading>
+    This is a test top-level heading
+  </Heading>
+  <Section>
+    <Heading>
+      This is a test heading for first section (H2)
+    </Heading>
+    <Heading>
+      This is also at level 2 but should have different aria-labelledby
+    </Heading>
+    <Article>
+      <Heading>
+        This is a test heading for an Article nested in a section (H3)
+      </Heading>
+    </Article>
+  </Section>
+  <Heading>
+    This is not nested, back to top-level
+  </Heading>
+  <Section>
+    <Heading>
+      This is nested to level 2
+    </Heading>
+    <Article>
+      <Heading>
+        This is nested to level 3
+      </Heading>
+      <Section>
+        <Heading>
+          This is nested to level 4
+        </Heading>
+        <Section>
+          <Heading>
+            This is nested to level 5
+          </Heading>
+          <Article>
+            <Heading>
+              This is nested to level 6
+            </Heading>
+          </Article>
+        </Section>
+      </Section>
+    </Article>
+  </Section>
+  <Heading>
+    This is not nested, back to top-level
+  </Heading>
+</main>
