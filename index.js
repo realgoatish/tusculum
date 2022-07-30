c1809dbed8871d6f790b185b5d3b8ac3c6123d30
Michael Lamb <mflambjr@gmail.com>
2022-07-29 22:31:48 -0400

    first subtree commit

---
+++ index.js
@@ -0,0 +1,3 @@
+export { default as Article } from './components/Article.svelte';
+export { default as ContentSection } from './components/Section.svelte';
+export { default as H } from './components/H.svelte';
