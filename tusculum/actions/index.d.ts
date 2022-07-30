498aa737fd21a83e5fe7a89e694e587e28296abf
Michael Lamb <mflambjr@gmail.com>
2022-07-29 22:30:07 -0400

    working on adding subtree

--- tusculum/actions/index.d.ts
+++
@@ -1,5 +0,0 @@
-/**
- * Create aria-labelledby relationship with Section.svelte/Article.svelte's first heading tag.
- * @param {HTMLElement} node
- */
-export function labelRegionWithHeading(node: HTMLElement, inBrowser: any): void;
