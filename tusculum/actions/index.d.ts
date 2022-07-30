9531c5f4d6dee3b0943250bcc88b08b3cbc0ed1e
Michael Lamb <mflambjr@gmail.com>
2022-07-29 22:31:48 -0400

    first subtree commit

---
+++ tusculum/actions/index.d.ts
@@ -0,0 +1,5 @@
+/**
+ * Create aria-labelledby relationship with Section.svelte/Article.svelte's first heading tag.
+ * @param {HTMLElement} node
+ */
+export function labelRegionWithHeading(node: HTMLElement, inBrowser: any): void;
---
+++ tusculum/constants/index.d.ts
@@ -0,0 +1,7 @@
+export namespace headingLevel {
+    const name: string;
+}
+export namespace counter {
+    const name_1: string;
+    export { name_1 as name };
+}
