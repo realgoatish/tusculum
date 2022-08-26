db7e01b68c97edf995f4293b366f3939a0b8d2a1
Michael Lamb <mflambjr@gmail.com>
2022-08-25 22:09:14 -0400

    update export name so this works

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,3 +1,2 @@
-export { default as Article } from './components/Article.svelte'
-export { default as ContentSection } from './components/Section.svelte'
+export { default as Section } from './components/Section.svelte'
 export { default as H } from './components/H.svelte'
