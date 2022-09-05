415e10bcdb8e21817ee454b84fe053c586b71f36
Michael Lamb <mflambjr@gmail.com>
2022-09-04 20:52:49 -0400

    change /env to /environment

--- src/lib/components/H.svelte
+++ src/lib/components/H.svelte
@@ -1,60 +1,54 @@
-<script lang='ts'>
-  import { getContext, afterUpdate } from 'svelte'
-  import { headingLevel, counter } from '$lib/constants/index.js'
-  import { browser } from '$app/env'
+<script lang="ts">
+	import { getContext, afterUpdate } from 'svelte';
+	import { headingLevel, counter } from '$lib/constants/index.js';
+	import { browser } from '$app/environment';
 
-  /** Set an optional class name for the top-level element of this component to enable 
-   * scoped styling of each component instance from outside (in parent components or pages)
-  */
-  export let wrapperClass: string|undefined = undefined
+	/** Set an optional class name for the top-level element of this component to enable
+	 * scoped styling of each component instance from outside (in parent components or pages)
+	 */
+	export let wrapperClass: string | undefined = undefined;
 
-  export let level: string|number|undefined = undefined
+	export let level: string | number | undefined = undefined;
 
-  let id = `h-${Math.floor((new Date() * Math.random()))}`
+	let id = `h-${Math.floor(new Date() * Math.random())}`;
 
-  // if user manually overrides the heading level with the prop, set it to that number
-  if (typeof level === 'number') {
-    // must be between 2 - 6
-    level = Math.max(2, Math.min(level, 6))
-    // if user did a manual override with a relative value, set it to that number
-  } else if (typeof level === 'string' && (level.startsWith("+") || level.startsWith("-"))) {
-    level = (getContext(headingLevel) || 2) + parseInt(level)
-    // if user's manual override is a string representation of a number, parse the int & set it to that number
-  } else if (typeof level === 'string') {
-    // must be between 2 - 6
-    level = Math.max(2, Math.min(parseInt(level), 6))
-    // if no manual override, get the context set by an ancestor
-  } else if (typeof getContext(headingLevel) === 'number') {
-    // must be between 2 - 6
-    level = Math.min(getContext(headingLevel), 6)
-  } else {
-    // if no override & no context was already set by an ancestor, initialize the context as 2
-    level = 2
-  }
+	// if user manually overrides the heading level with the prop, set it to that number
+	if (typeof level === 'number') {
+		// must be between 2 - 6
+		level = Math.max(2, Math.min(level, 6));
+		// if user did a manual override with a relative value, set it to that number
+	} else if (typeof level === 'string' && (level.startsWith('+') || level.startsWith('-'))) {
+		level = (getContext(headingLevel) || 2) + parseInt(level);
+		// if user's manual override is a string representation of a number, parse the int & set it to that number
+	} else if (typeof level === 'string') {
+		// must be between 2 - 6
+		level = Math.max(2, Math.min(parseInt(level), 6));
+		// if no manual override, get the context set by an ancestor
+	} else if (typeof getContext(headingLevel) === 'number') {
+		// must be between 2 - 6
+		level = Math.min(getContext(headingLevel), 6);
+	} else {
+		// if no override & no context was already set by an ancestor, initialize the context as 2
+		level = 2;
+	}
 
-  let currentCounterValue = getContext(counter)
+	let currentCounterValue = getContext(counter);
 
-  // when this comp is first instantiated, the store is set to 1
-  $: if (browser && $currentCounterValue === 0) {
-      $currentCounterValue = 1
-    }
+	// when this comp is first instantiated, the store is set to 1
+	$: if (browser && $currentCounterValue === 0) {
+		$currentCounterValue = 1;
+	}
 
-  // any time this component's DOM is updated, set the store back to 0 to trigger
-  // its first ancestor <Section> syncing its 'aria-labelledby' with this heading's 'id'.
-  // This protects against the two getting out of sync due to obscure DOM changes.
-  afterUpdate(() => {
-    if (browser && $currentCounterValue) {
-      $currentCounterValue = 0
-    }
-  })
-
+	// any time this component's DOM is updated, set the store back to 0 to trigger
+	// its first ancestor <Section> syncing its 'aria-labelledby' with this heading's 'id'.
+	// This protects against the two getting out of sync due to obscure DOM changes.
+	afterUpdate(() => {
+		if (browser && $currentCounterValue) {
+			$currentCounterValue = 0;
+		}
+	});
 </script>
 
-<svelte:element 
-  this={`h${level}`} 
-  {id}
-  class:wrapperClass
->
-  <slot />
+<svelte:element this={`h${level}`} {id} class:wrapperClass>
+	<slot />
 </svelte:element>
-
--- src/lib/components/Section.svelte
+++ src/lib/components/Section.svelte
@@ -1,68 +1,62 @@
+<script lang="ts">
+	import { setContext, getContext } from 'svelte';
+	import { headingLevel, counter } from '$lib/constants/index.js';
+	import { labelRegionWithHeading } from '$lib/actions/index.js';
+	import { writable } from 'svelte/store';
+	import { browser } from '$app/environment';
 
-<script lang='ts'>
-  import { setContext, getContext } from 'svelte'
-  import { headingLevel, counter } from '$lib/constants/index.js'
-  import { labelRegionWithHeading } from '$lib/actions/index.js'
-  import { writable } from 'svelte/store'
-  import { browser } from '$app/env'
+	/** Set an optional class name for the top-level element of this component to enable
+	 * scoped styling of each component instance from outside (in parent components or pages)
+	 */
+	export let wrapperClass: string | undefined = undefined;
 
+	export let level: string | number | undefined = undefined;
 
-  /** Set an optional class name for the top-level element of this component to enable 
-   * scoped styling of each component instance from outside (in parent components or pages)
-  */
-  export let wrapperClass: string|undefined = undefined
+	let store = writable(0);
 
-  export let level: string|number|undefined = undefined
+	// if user did a manual override with a number, set the context to that
+	if (typeof level === 'number') {
+		// must be between 2 - 6
+		level = Math.min(Math.max(level, 2), 6);
+		setContext(headingLevel, level);
+		setContext(counter, store);
 
-  let store = writable(0)
+		// if user did a manual override with a relative value, parse the int & set it to that number
+	} else if (typeof level === 'string' && (level.startsWith('+') || level.startsWith('-'))) {
+		level = (getContext(headingLevel) || 2) + parseInt(level);
+		setContext(headingLevel, level);
+		setContext(counter, store);
 
-  // if user did a manual override with a number, set the context to that
-  if (typeof level === 'number') {
-    // must be between 2 - 6
-    level = Math.min(Math.max(level, 2), 6)
-    setContext(headingLevel, level)
-    setContext(counter, store)
+		// if user's manual override is a string representation of a number, parse the int & set it to that number
+	} else if (typeof level === 'string') {
+		// must be between 2 - 6
+		level = Math.min(Math.max(parseInt(level), 2), 6);
+		setContext(headingLevel, level);
+		setContext(counter, store);
 
-    // if user did a manual override with a relative value, parse the int & set it to that number
-  } else if (typeof level === 'string' && (level.startsWith("+") || level.startsWith("-"))) {
-    level = (getContext(headingLevel) || 2) + parseInt(level)
-    setContext(headingLevel, level)
-    setContext(counter, store)
+		// if no prop was supplied, look for a context that was already set from an ancestor DocumentSection component
+	} else if (typeof getContext(headingLevel) === 'number') {
+		level = getContext(headingLevel) + 1;
+		setContext(headingLevel, level);
+		setContext(counter, store);
+	} else {
+		// if no prop was supplied & no context was already set by an ancestor, initialize the context as 2
+		level = 2;
+		setContext(headingLevel, level);
+		setContext(counter, store);
+	}
 
-    // if user's manual override is a string representation of a number, parse the int & set it to that number
-  } else if (typeof level === 'string') {
-    // must be between 2 - 6
-    level = Math.min(Math.max(parseInt(level), 2), 6)
-    setContext(headingLevel, level)
-    setContext(counter, store)
+	let currentCounterValue = getContext(counter);
 
-    // if no prop was supplied, look for a context that was already set from an ancestor DocumentSection component
-  } else if (typeof getContext(headingLevel) === 'number') {
-    level = getContext(headingLevel) + 1
-    setContext(headingLevel, level)
-    setContext(counter, store)
-  } else {
-    // if no prop was supplied & no context was already set by an ancestor, initialize the context as 2
-    level = 2
-    setContext(headingLevel, level)
-    setContext(counter, store)
-  }
+	let section = null;
 
-  let currentCounterValue = getContext(counter)
-
-  let section = null
-
-  // whenever this store value changes, run the action to update this <Section>'s
-  // 'aria-labelledby' attribute with its first descendant <H>'s 'id'.
-  // this keeps the two in sync even when that <H>'s DOM gets updated independently
-  // i.e. if its 'id' attribute changes, you don't have to worry about it
-  $: $currentCounterValue, labelRegionWithHeading(section, browser)
-
+	// whenever this store value changes, run the action to update this <Section>'s
+	// 'aria-labelledby' attribute with its first descendant <H>'s 'id'.
+	// this keeps the two in sync even when that <H>'s DOM gets updated independently
+	// i.e. if its 'id' attribute changes, you don't have to worry about it
+	$: $currentCounterValue, labelRegionWithHeading(section, browser);
 </script>
 
-<section
-  bind:this={section}
-  class:wrapperClass
->
-  <slot />
+<section bind:this={section} class:wrapperClass>
+	<slot />
 </section>
